5995d9687eb9ccbf3af71dc09099d59c072c2140
GWG <gwalle.gza@gmail.com>
2022-02-04 12:43:10 -0600

    add s3 client

--- backend/src/utils/s3.ts
+++ backend/src/utils/s3.ts
@@ -1,6 +1,6 @@
 
-import S3 from 'aws-sdk/clients/s3';
-import { AWS_CONFIG } from '../constants';
+import S3 from 'aws-sdk/clients/s3.js';
+import { AWS_CONFIG } from '../constants/index.js';
 
 // https://docs.aws.amazon.com/AWSJavaScriptSDK/v3/latest/clients/client-s3/modules/credentials.html
 
