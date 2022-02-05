6893a8dc73248b02e504ce3493c166d05c55a25c
GWG <gwalle.gza@gmail.com>
2022-02-04 21:18:55 -0600

    add users and properties routers to app file

--- backend/src/app.ts
+++ backend/src/app.ts
@@ -1,22 +1,27 @@
-import express from 'express';
-import {startServer} from './utils/server.js';
-import { ServerConfig } from './interfaces/server';
-import {setMongoConnection} from './middleware/db.js';
+import express from "express";
+import { startServer } from "./utils/server.js";
+import { ServerConfig } from "./interfaces/server";
+import { setMongoConnection } from "./middleware/db.js";
 import { PORT } from "./constants/index.js";
-import 'dotenv/config';
+import "dotenv/config";
 
 // Routes
-import propertiesRoutes from './routes/properties.js';
+import propertiesRoutes from "./routes/properties.js";
+import usersRoutes from "./routes/users.js";
 
 const app = express();
 
+app.use(express.urlencoded({extended: true}));
+app.use(express.json()) 
 // Connect to mongodb
 app.use(setMongoConnection);
 
-app.use('/properties', propertiesRoutes);
+app.use("/users", usersRoutes);
+app.use("/properties", propertiesRoutes);
 
-startServer((config: ServerConfig) => {
-    app.listen(config.port);    
-},{port: PORT});
-
- 
+startServer(
+  (config: ServerConfig) => {
+    app.listen(config.port);
+  },
+  { port: PORT }
+);
