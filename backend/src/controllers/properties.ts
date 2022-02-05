c2f5bd11614026e718fd8e162cdfa6173d9e4f30
GWG <gwalle.gza@gmail.com>
2022-02-04 21:18:08 -0600

    add properties basic endpoints

--- backend/src/controllers/properties.ts
+++ backend/src/controllers/properties.ts
@@ -1,22 +1,24 @@
 import { Request, Response, NextFunction } from "express";
 import { UploadParams } from "../interfaces/aws/index.js";
 import { UPLOAD_DIR } from "../constants/index.js";
-import fs from 'fs';
+import { ObjectId } from "mongodb";
+import fs from "fs";
 import { client } from "../utils/s3.js";
-import {upload} from '../helpers/aws/index.js';
-import path from 'path';
-import {Property} from '../models/property.js';
+import path from "path";
+import { Property } from "../models/property.js";
 
-export const uploadPicturesToS3 = async (
+export const saveProperty = async (
   req: Request,
   res: Response,
   next: NextFunction
 ) => {
-  const userAddress = req.body.userAddress;
+  const userAddress = req.body.ownerAddress;
+  const city = req.body.city;
+  const nftToken = req.body.nftToken;
+  const country = req.body.country;
   const files: any = req.files;
-  console.log('the files', files)
+
   const attachments: [UploadParams] = files.map((elem: any) => {
-    console.log('elem ', elem)
     if (
       !elem.mimetype.includes("jpeg") &&
       !elem.mimetype.includes("jpg") &&
@@ -24,14 +26,12 @@ export const uploadPicturesToS3 = async (
     ) {
       const error = new Error(
         "Please submit one of the following formats: .jpeg, .jpg, .png"
-      );      
+      );
       return next(error);
     }
     const fileStream = fs.createReadStream(elem.path);
     fileStream.on("error", function (err) {
-      const error = new Error(
-        "Could not upload file, please try again"
-      );
+      const error = new Error("Could not upload file, please try again");
       return next(error);
     });
     return {
@@ -42,25 +42,40 @@ export const uploadPicturesToS3 = async (
     };
   });
   const responses = await Promise.all(
-    attachments.map((param) =>  client.upload(param).promise()));
+    attachments.map((param) => client.upload(param).promise())
+  );
   if (!responses) {
-    const error = new Error("Could not upload file, please try again")
+    const error = new Error("Could not upload file, please try again");
     return next(error);
   }
-  console.log('the responses ',responses)
   // Delete files
   fs.readdir(UPLOAD_DIR, (err, files) => {
     if (err) throw err;
     for (const file of files) {
-      fs.unlink(path.join(UPLOAD_DIR, file), err => {
+      fs.unlink(path.join(UPLOAD_DIR, file), (err) => {
         if (err) throw err;
       });
     }
   });
-  const links = responses.map(elem => {
+  const links = responses.map((elem) => {
     return elem?.Location;
   });
-  const newProperty = new Property(userAddress, links);
+  const newProperty = new Property(userAddress, links, nftToken, city, country);
   const savedProperty = await newProperty.save();
-  return res.status(204).json({property: savedProperty});
+  return res.status(201).json({ propertyId: savedProperty.insertedId });
+};
+
+export const fetchPropertyById = async (req: Request, res: Response) => {
+  const propertyId = new ObjectId(req.params.propertyId);
+
+  const property = await Property.findById(propertyId);
+  return res.status(200).json(property);
+};
+
+export const fetchPropertiesByLocation = async (req: Request, res: Response) => {
+  const query = {...req.query};
+  console.log('the query ',req.query)
+  const property = await Property.findManyByLocation(query);
+  console.log(property)
+  return res.status(200).json(property);
 };
--- backend/src/routes/properties.ts
+++ backend/src/routes/properties.ts
@@ -1,11 +1,13 @@
-import express from 'express';
-import { uploadPictures } from '../middleware/pictures.js';
-import { uploadPicturesToS3 } from '../controllers/properties.js';
+import express from "express";
+import { saveProperty, fetchPropertiesByLocation, fetchPropertyById } from "../controllers/properties.js";
 import multer from "multer";
+
 const upload = multer({ dest: "uploads/" });
 
 const router = express.Router();
 
-router.post('/:propertyId', upload.array("files", 10), uploadPicturesToS3);
+router.post("", upload.array("files", 10), saveProperty);
+router.get("", fetchPropertiesByLocation);
+router.get("/:propertyId", fetchPropertyById);
 
 export default router;
