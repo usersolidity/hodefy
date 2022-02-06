cd3c9cb3b744e86f5acb15d40d8029f5c44c3803
Philip Lu <philiplu97@gmail.com>
2022-02-06 03:16:41 -0500

    Oracle stuff
    Add GET request for relevant property information in order to mint an NFT

--- contracts/hodefy.sol
+++ contracts/hodefy.sol
@@ -5,12 +5,15 @@ import "@chainlink/contracts/src/v0.8/ChainlinkClient.sol";
 
 pragma solidity >=0.7.0;
 
-contract Hodefy is ChainlinkClient {
-    using Chainlink for Chainlink.Request; // Chainlink will handle getting price feeds and GET requests to our Backend API
+contract Hodefy is ChainlinkClient { // Chainlink will handle getting price feeds and GET requests to our Backend API
+    using Chainlink for Chainlink.Request;
     
-    address private oracle; // For use with Chainlink Oracle
     address internal hodefy = 0xcD0308D313678777820010d0467430362936F57b; // Address of my (Phil's) ETH wallet is the Hodefy admin
     // Replace with official Hodefy company wallet later
+
+    address private oracle; // For use with Chainlink Oracle on Polygon Mumbai Testnet
+    bytes32 private jobId; // ID of the job we'll give to the Oracle
+    uint256 private fee; // Fee paid for Oracle services
 
     uint256 internal propertyID;
      // Largest property ID
@@ -32,7 +35,10 @@ contract Hodefy is ChainlinkClient {
 
     constructor() {
         setPublicChainlinkToken();
-        oracle = 0xc57B33452b4F7BB189bB5AfaE9cc4aBa1f7a4FD8; // Kovan network Chainlink Oracle
+        // Values accessed from https://docs.polygon.technology/docs/develop/oracles/chainlink/#code-example
+        oracle = 0x58BBDbfb6fca3129b91f0DBE372098123B38B5e9; // Polygon Mumbai network Chainlink Oracle
+        jobId = "a82495a8fd5b4cb492b17dc0cc31a4fe"; // HTTP GET request receiving a JSON bytes32 (string) result of Property information
+        fee = 0.1 * 10 ** 18; // 0.1 LINK fee
     }
 
     function setFees(uint8 percent) private onlyHodefy {
@@ -48,7 +54,45 @@ contract Hodefy is ChainlinkClient {
     // We want a function that can take an address owner and verified property information and create a new Property NFT by minting 
     // using the verified info. We also need a secure process to transmit data from verified legal documents proving ownership starting
     // from somewhere off-chain since having both an NFT and a bunch of legal information stored on-chain will be very expensive.
+    function requestPropertyData() public onlyHodefy returns(bytes32 requestId) {
+        Chainlink.Request memory request = buildChainlinkRequest(jobId, address(this), this.fulfill.selector);
 
+        request.add("get", "http://localhost:4000");
+
+        request.add("path", "1.project.ownerAddress");
+        request.add("path", "1.project.streetAddress");
+        request.add("path", "1.project.city");
+        request.add("path", "1.project.country");
+        request.add("path", "1.project.link");
+
+        return sendChainlinkRequestTo(oracle, request, fee);
+    }
+
+    // Like the name says, convert a bytes32 variable to a string one
+    function bytes32ToString(bytes32 _bytes32) public pure returns (string memory) {
+        uint8 i = 0;
+        while(i < 32 && _bytes32[i] != 0) {
+            i++;
+        }
+        bytes memory bytesArray = new bytes(i);
+        for (i = 0; i < 32 && _bytes32[i] != 0; i++) {
+            bytesArray[i] = _bytes32[i];
+        }
+        return string(bytesArray);
+    }
+
+    // Request all necessary Property data in order to mint a corresponding NFT
+    function fulfill(
+        bytes32 _requestId, 
+        bytes32 ownerAddress, 
+        bytes32 city, 
+        bytes32 country, 
+        bytes32 streetAddress, 
+        bytes32 file
+        ) public recordChainlinkFulfillment(_requestId)
+    {
+
+    }
 
 }
 
